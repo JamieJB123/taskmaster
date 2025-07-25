bbe6fad17030e5c29d444d2e4f1585e6e6ece8ca
Jshnee <203004294+JamieJB123@users.noreply.github.com>
2025-07-25 10:22:09 +0100

    Set up views / templates / settings to display task data in to-do & done tables

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Task
 
 def home(request):
-    return render(request, 'tasks/index.html')
+    # Fetch tasks based on their completion status
+    to_do_tasks = Task.objects.filter(completed_status=False).order_by("due_date")
+    done_tasks = Task.objects.filter(completed_status=True).order_by("due_date")
+
+    # Pass the tasks to the template
+    context = {
+        'to_do_tasks': to_do_tasks,
+        'done_tasks': done_tasks,
+    }
+    return render(request, 'tasks/index.html', context)
