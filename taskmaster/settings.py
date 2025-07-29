d1b6865db0d631724c5216e4e66f03308af02136
Jshnee <203004294+JamieJB123@users.noreply.github.com>
2025-07-29 13:40:08 +0100

    Add task form to homepage to enable users to add tasks to the database and task-table

--- taskmaster/settings.py
+++ taskmaster/settings.py
@@ -44,7 +44,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tasks',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+#Specifying crispy forms template pack
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
 def home(request):
+    """Logic for the 'task form'"""
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            # Save the new task to the database
+            new_task = form.save(commit=False)
+            new_task.completed_status = False # Default to not completed
+            new_task.save()
+            return redirect('home') # Redirect to the home page after saving
+    else:
+            form = TaskForm() # Reinitialize the form if it's not valid
+
+
     # Fetch tasks based on their completion status
     to_do_tasks = Task.objects.filter(completed_status=False).order_by("due_date")
     done_tasks = Task.objects.filter(completed_status=True).order_by("due_date")
 
-    # Pass the tasks to the template
+    # Pass the form and tasks to the template
     context = {
+        'form': form,  # The TaskForm instance
         'to_do_tasks': to_do_tasks,
         'done_tasks': done_tasks,
     }
