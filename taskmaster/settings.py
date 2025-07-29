b5e93e5dac822f039eaeea5a7a932e0be451b482
Jshnee <203004294+JamieJB123@users.noreply.github.com>
2025-07-29 15:39:50 +0100

    Update settings for deployment

--- taskmaster/settings.py
+++ taskmaster/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY',)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["ci-taskmaster-app-8aa6c016d1b3.herokuapp.com",]
 
 
 # Application definition
